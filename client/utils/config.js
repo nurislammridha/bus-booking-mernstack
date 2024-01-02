ae31e14b176a42a63d313da3b8f308fdff4decd8
nurislammridha <mridha851@gmail.com>
2024-01-02 14:05:52 +0600

    build

--- client/utils/config.js
+++ client/utils/config.js
@@ -1,2 +1,3 @@
-export const API = 'http://localhost:8086/api';
+export const API_TEST = 'http://localhost:8086/api';
+export const API = 'https://bus-booking-mern.onrender.com/api';
 export const API_ROOT = 'http://localhost:8086';
