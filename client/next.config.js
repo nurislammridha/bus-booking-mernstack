635768ea1ccd7b2dcdde17dfe29f016e9468ef11
nurislammridha <mridha851@gmail.com>
2024-01-03 13:20:57 +0600

    de

--- client/next.config.js
+++ client/next.config.js
@@ -1,23 +1,4 @@
-const withCSS = require('@zeit/next-css');
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
-function HACK_removeMinimizeOptionFromCssLoaders(config) {
-  console.warn(
-    'HACK: Removing `minimize` option from `css-loader` entries in Webpack config',
-  );
-  config.module.rules.forEach(rule => {
-    if (Array.isArray(rule.use)) {
-      rule.use.forEach(u => {
-        if (u.loader === 'css-loader' && u.options) {
-          delete u.options.minimize;
-        }
-      });
-    }
-  });
-}
-
-module.exports = withCSS({
-  webpack(config) {
-    HACK_removeMinimizeOptionFromCssLoaders(config);
-    return config;
-  },
-});
+module.exports = nextConfig
